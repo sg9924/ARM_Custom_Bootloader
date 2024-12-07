95c2601d31ed26913a39fa866e38fd32b1cf29be
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-12-07 17:47:13 +0530

    stm32f407 | Source Code | Host App (3)
    Serial Communication and ports utility

--- Source Code/Host Application/STM32F407_Programmer_Application.py
+++ Source Code/Host Application/STM32F407_Programmer_Application.py
@@ -46,3 +46,67 @@ def get_crc(buff, length):
                 crc = (crc << 1)
     return crc
 #----------------------------------------------------------------------------------
+
+#----------------------------- Serial Port ----------------------------------------
+#1. Lists the serial ports available on the Host system
+def serial_ports():
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+#2. Serial Port Configuration
+def Serial_Port_Configuration(port):
+    global ser
+    try:
+        ser = serial.Serial(port,115200,timeout=2)
+    except:
+        print("\n[ERROR]: Not a valid Port!!")
+        
+        port = serial_ports()
+        if(not port):
+            print("\n[ERROR]: No Ports Detected!!")
+        else:
+            print("\n[INFO]: Available Ports on the System,")
+            print("\n",port)
+        return -1
+    if ser.is_open:
+        print("\n[INFO]: Port Open Success")
+    else:
+        print("\n[ERROR]: Port Open Failed!!")
+    return 0
+
+#3. Read Serial Port
+def read_serial_port(length):
+    read_value = ser.read(length)
+    return read_value
+
+#4. Close Serial Port
+def Close_serial_port():
+    pass
+
+#5. Purge Serial Port
+def purge_serial_port():
+    ser.reset_input_buffer()
+
+#6. Write to Serial Port
+def Write_to_serial_port(value, *length):
+        data = struct.pack('>B', value)
+        if (verbose_mode):
+            value = bytearray(data)
+            print("   "+"0x{:02x}".format(value[0]),end=' ')
+        if(mem_write_active and (not verbose_mode)):
+                print("#",end=' ')
+        ser.write(data)
+#----------------------------------------------------------------------------------
