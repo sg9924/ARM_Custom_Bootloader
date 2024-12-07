97398314d91cedd4194ed691a001a494a511bd0c
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-12-07 17:33:13 +0530

    stm32f407 | Source Code | Host Application
    Python Host Application - Initial Code

---
+++ Source Code/Host Application/STM32F407_Programmer_Application.py
@@ -0,0 +1,27 @@
+import serial
+import struct
+import os
+import sys
+import glob
+
+
+#Flash
+Flash_HAL_OK                                        = 0x00
+Flash_HAL_ERROR                                     = 0x01
+Flash_HAL_BUSY                                      = 0x02
+Flash_HAL_TIMEOUT                                   = 0x03
+Flash_HAL_INV_ADDR                                  = 0x04
+
+
+#Bootloader Commands
+COMMAND_BL_GET_VER                                  =0x51
+COMMAND_BL_GET_HELP                                 =0x52
+
+
+#Packet Length Details of Bootloader Commands
+COMMAND_BL_GET_VER_LEN                              =6
+COMMAND_BL_GET_HELP_LEN                             =6
+
+
+verbose_mode       =1
+mem_write_active   =0
