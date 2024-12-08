1441352aa109c1eccb6e19a7fb3ccd90078baca1
sg9924 <170335278+sg9924@users.noreply.github.com>
2024-12-08 17:39:32 +0530

    stm32f407 | Source Code | main.h
    Adding macros and function prototypes

--- Source Code/main.h
+++ Source Code/main.h
@@ -101,10 +101,26 @@ void Error_Handler(void);
 #define BL_ACK   0XA5
 #define BL_NACK  0X7F
 
+//RAM
+#define SRAM1_SIZE             112*1024                   //112KB
+#define SRAM1_END              (SRAM1_BASE + SRAM1_SIZE)
+#define SRAM2_SIZE             16*1024                    //16KB
+#define SRAM2_END              (SRAM2_BASE + SRAM2_SIZE)
+#define FLASH_SIZE             512*1024                   //512KB
+#define BKPSRAM_SIZE           4*1024                     //4KB
+#define BKPSRAM_END            (BKPSRAM_BASE + BKPSRAM_SIZE)
+
 //Bootloader Command definition
 #define BL_GET_VER				      0x51
 #define BL_GET_HELP				      0x52
 #define BL_GET_CID				      0x53
+#define BL_GET_RDP_STATUS		    0x54
+#define BL_GO_TO_ADDR			      0x55
+#define BL_FLASH_ERASE          0x56
+#define BL_MEM_WRITE			      0x57
+#define BL_EN_RW_PROTECT		    0x58
+#define BL_MEM_READ				      0x59
+#define BL_READ_SECTOR_P_STATUS	0x5A
 
 /* USER CODE END Private defines */
 
@@ -125,10 +141,27 @@ void bootloader_send_nack(void);
 uint8_t bootloader_verify_crc (uint8_t *pData, uint32_t len,uint32_t crc_host);
 
 uint8_t get_bootloader_version(void);
+uint16_t get_mcu_chip_id(void);
+uint8_t get_flash_rdp_level(void);
+uint8_t verify_address(uint32_t go_address);
+uint8_t execute_flash_erase(uint8_t sector_number , uint8_t number_of_sector);
+uint8_t execute_mem_write(uint8_t *pBuffer, uint32_t mem_address, uint32_t len);
+uint8_t configure_flash_sector_rw_protection(uint8_t sector_details, uint8_t protection_mode, uint8_t disable);
+uint16_t read_OB_rw_protection_status(void);
 
 //3. Bootloader Handle Command Functions
 void bootloader_handle_getver_cmd(uint8_t *bl_rx_buffer);
 void bootloader_handle_gethelp_cmd(uint8_t *bl_rx_buffer);
+void bootloader_handle_getcid_cmd(uint8_t *pBuffer);
+void bootloader_handle_getrdp_cmd(uint8_t *pBuffer);
+void bootloader_handle_go_cmd(uint8_t *pBuffer);
+void bootloader_handle_flash_erase_cmd(uint8_t *pBuffer);
+void bootloader_handle_mem_write_cmd(uint8_t *pBuffer);
+void bootloader_handle_en_rw_protect(uint8_t *pBuffer);
+void bootloader_handle_mem_read (uint8_t *pBuffer);
+void bootloader_handle_read_sector_protection_status(uint8_t *pBuffer);
+void bootloader_handle_read_otp(uint8_t *pBuffer);
+void bootloader_handle_dis_rw_protect(uint8_t *pBuffer);
 
 
 #endif /* __MAIN_H */
